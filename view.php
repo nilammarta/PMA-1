d572095158378196dd5d633747fb5e834bc03dfd
Nilam <nilammartaanggreni@gmail.com>
2024-01-15 13:56:03 +0800

    temp

--- view.php
+++ view.php
@@ -21,58 +21,7 @@ showHeader("persons");
 ?>
     <main>
       <section class="main-section d-flex flex-row">
-        <div class="sidebar d-none d-lg-flex flex-column gap-5">
-          <div class="sidebar-content">
-            <nav class="main-nav">
-              <ul class="main-nav-list">
-                <li class="nav-item">
-                  <a class="main-nav-link" href="dashboard.php">
-                    <ion-icon
-                      name="file-tray-full-outline"
-                      class="nav-icon"
-                    ></ion-icon>
-                    Dashboard
-                  </a>
-                </li>
-                <li class="nav-item nav-open">
-                  <a class="main-nav-link" href="persons.php">
-                    <ion-icon
-                      name="people-outline"
-                      class="nav-icon"
-                    ></ion-icon>
-                    Persons
-                  </a>
-                </li>
-              </ul>
-            </nav>
-
-            <div class="profile">
-              <h6 class="heading-6 sub-heading m-0">Account</h6>
-              <nav class="main-nav">
-                <ul class="main-nav-list">
-                  <li class="nav-item">
-                    <a class="main-nav-link" href="my-profile.php">
-                      <ion-icon
-                        name="person-circle-outline"
-                        class="nav-icon"
-                      ></ion-icon>
-                      My Profile
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="main-nav-link cta" href="logout.php">
-                      <ion-icon
-                        name="log-out-outline"
-                        class="nav-icon"
-                      ></ion-icon>
-                      Logout
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-        </div>
+        <?php showSidebar("persons"); ?>
 
         <div class="main-content">
           <div class="profile m-3 m-md-4">
